07473db776b752209474b9e9d49c331c9076c02d
Marco Habegger <taysumimarco@gmail.com>
2018-02-16 21:00:33 +0100

    update assembly infos

--- Sources/Habbi.Utils/Properties/AssemblyInfo.cs
+++ Sources/Habbi.Utils/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Habbi.Utils")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A collection of utilities I collected.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Habbi.Utils")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
+[assembly: AssemblyCopyright("Copyright © Marco Habegger 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
